5c0d1b64d3984dd6f629f4f6c0f949e164de9361
Ismatulla <ismatilloqoyliyev@gmail.com>
2023-02-16 16:42:17 +0100

    initiating-project

---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+import App from './components/App';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
